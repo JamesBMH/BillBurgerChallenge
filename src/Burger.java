54ba5dd501a4894e07b0a8b5ddea9c8c6ef897ea
JamesBMH <jameshertzog3@gmail.com>
2025-03-24 21:31:26 +0200

    Added Topping class in Burger.java. Implemented addTopping function for burger class. Implemented toString's. Tested.

--- src/Burger.java
+++ src/Burger.java
@@ -1,17 +1,68 @@
 public class Burger {
-    Burger(){
+    public Burger(){
         this.type = "Cheese Burger";
         this.price = 45;
     }
 
-    Burger(String type, double price){
+    public Burger(String type, double price){
         this.type = type;
         this.price = price;
     }
 
+    public void addTopping(String type, double price){
+        if (topping1 == null){
+            topping1 = new Topping(type, price);
+            this.price += price;
+        } else if (topping2 == null) {
+            topping2 = new Topping(type, price);
+            this.price += price;
+        } else if (topping3 == null) {
+            topping3 = new Topping(type, price);
+            this.price += price;
+        } else {
+            System.out.println("No more toppings allowed.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        output += String.format("Burger: %s%n", type);
+        output += String.format(" Price: R%.2f%n", price);
+
+        if (!(topping1 == null)){
+            output += topping1.toString();
+        }
+        if (!(topping2 == null)){
+            output += topping2.toString();
+        }
+        if (!(topping3 == null)){
+            output += topping3.toString();
+        }
+        return output;
+    }
+
+    protected String type;
+    protected double price;
+    protected Topping topping1 = null;
+    protected Topping topping2 = null;
+    protected Topping topping3 = null;
+}
+
+class Topping {
+    public Topping(String type, double price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        output += String.format("Topping: %s%n", type);
+        output += String.format(" Price: %.2f%n", price);
+        return output;
+    }
+
     private String type;
     private double price;
-//    private Topping topping1;
-//    private Topping topping2;
-//    private Topping topping3;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Burger burger = new Burger();
+        System.out.println(burger.toString());
+
+        Burger CheeseBurger = new Burger("Cheese Burger", 55.45);
+        CheeseBurger.addTopping("Cheese", 3.99);
+        CheeseBurger.addTopping("Gherkins", 2.99);
+        CheeseBurger.addTopping("Onions", 4.37);
+        // Should complain about this 4'th topping
+        CheeseBurger.addTopping("Mayo", 1.50);
+
+        System.out.println(CheeseBurger.toString());
     }
 }
