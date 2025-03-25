89dd3e13fe02a0319a1501468016de7d362aaf3a
JamesBMH <jameshertzog3@gmail.com>
2025-03-25 11:55:27 +0200

    Added the Deluxe Burger with appropriate override functions. Added special toString function for topping class since DeluxeBurger extra toppings don't alter price. Tested.

--- src/Burger.java
+++ src/Burger.java
@@ -1,7 +1,7 @@
 public class Burger {
     public Burger(){
         this.type = "Cheese Burger";
-        this.price = 45;
+        this.price = 44.99;
     }
 
     public Burger(String type, double price){
@@ -28,7 +28,7 @@ public class Burger {
     public String toString() {
         String output = "";
         output += String.format("Burger: %s%n", type);
-        output += String.format(" Price: R%.2f%n", price);
+        output += String.format("  Price: R%.2f%n", price);
 
         if (!(topping1 == null)){
             output += topping1.toString();
@@ -49,20 +49,82 @@ public class Burger {
     protected Topping topping3 = null;
 }
 
+// If a burger already has a topping and it is added it will count as a double topping
+// Ex. a cheese burger with cheese added will have 2 slices of cheese
 class Topping {
     public Topping(String type, double price) {
         this.type = type;
         this.price = price;
     }
 
-    @Override
     public String toString() {
         String output = "";
-        output += String.format("Topping: %s%n", type);
-        output += String.format(" Price: %.2f%n", price);
+        output += String.format(" Topping: %s%n", type);
+        output += String.format("  Price: %.2f%n", price);
+        return output;
+    }
+
+// Deluxe burger extra toppings do not affect price, requires own toString
+    public String toStringDeluxe(){
+        String output = "";
+        output += String.format(" Topping: %s%n", type);
         return output;
     }
 
     private String type;
     private double price;
+}
+
+
+class DeluxeBurger extends Burger {
+    public DeluxeBurger() {
+        type = "Deluxe Burger";
+        price = 79.99;
+    }
+
+    @Override
+    public void addTopping(String type, double price) {
+        if (topping1 == null){
+            topping1 = new Topping(type, price);
+        } else if (topping2 == null) {
+            topping2 = new Topping(type, price);
+        } else if (topping3 == null) {
+            topping3 = new Topping(type, price);
+        } else if (topping4 == null) {
+            topping4 = new Topping(type, price);
+        } else if (topping5 == null) {
+            topping5 = new Topping(type, price);
+        } else {
+            System.out.println("No more toppings allowed.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+
+        output += String.format("Burger: %s%n", type);
+        output += String.format("  Price: R%.2f%n", price);
+
+        if (!(topping1 == null)){
+            output += topping1.toStringDeluxe();
+        }
+        if (!(topping2 == null)){
+            output += topping2.toStringDeluxe();
+        }
+        if (!(topping3 == null)){
+            output += topping3.toStringDeluxe();
+        }
+        if (!(topping4 == null)){
+            output += topping4.toStringDeluxe();
+        }
+        if (!(topping5 == null)){
+            output += topping5.toStringDeluxe();
+        }
+
+        return output;
+    }
+
+    private Topping topping4 = null;
+    private Topping topping5 = null;
 }
--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,20 @@ public class Main {
         CheeseBurger.addTopping("Gherkins", 2.99);
         CheeseBurger.addTopping("Onions", 4.37);
         // Should complain about this 4'th topping
-        CheeseBurger.addTopping("Mayo", 1.50);
+        CheeseBurger.addTopping("Mayo", 1.49);
 
         System.out.println(CheeseBurger.toString());
+
+        Burger deluxeBurger = new DeluxeBurger();
+
+        deluxeBurger.addTopping("Cheese", 3.99);
+        deluxeBurger.addTopping("Onions", 2.99);
+        deluxeBurger.addTopping("Relish", 2.75);
+        deluxeBurger.addTopping("BBQ Sauce", 4.99);
+        deluxeBurger.addTopping("Mayo", 1.49);
+        // Should complain about 6'th topping
+        deluxeBurger.addTopping("Gherkins", 2.99);
+
+        System.out.println(deluxeBurger.toString());
     }
 }
